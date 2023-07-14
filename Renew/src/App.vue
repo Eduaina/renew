96d1e45ad6edeaac0f5be24a590ad51fcbfe9d8b
Eduaina <eduaighalo29@gmail.com>
2023-07-14 06:02:46 +0100

    my first vue template

---
+++ Renew/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+<h4> Current count is...</h4>
+<h1>{{ count }}</h1>
+<button @click="count++">+</button>
+<button @click="count--">-</button>
+  </div>
+ 
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
